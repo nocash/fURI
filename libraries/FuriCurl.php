285820da14cde48b1c4663a7781a0f96d300d019
nocash <z727090@gmail.com>
2009-05-24 01:47:10 -0400

    adjusting curl class to match abstract; removed methods

--- libraries/FuriCurl.php
+++ libraries/FuriCurl.php
@@ -21,72 +21,17 @@ class FuriCurl_Core extends FuriAbstract implements FuriInterface {
 
 // ============================================================================
 
-	public function get($url = NULL)
 	{
-		$this->setopt(CURLOPT_HTTPGET, TRUE);
-		return $this->exec();
-	}
 
-	public function post($url = NULL)
-	{
-		$this->setopt(CURLOPT_POST, TRUE);
-		return $this->exec();
-	}
 
-	public function put($url = NULL)
-	{
-		$this->setopt(CURLOPT_PUT, TRUE);
-		return $this->exec();
-	}
 
-	public function delete($url = NULL)
-	{
-		$this->setopt(CURLOPT_CUSTOMREQUEST, 'DELETE');
-		return $this->exec();
-	}
 
-	// @todo move to abstract?
-	public function copy_cookies()
-	{
-		if ( is_array($_COOKIE) )
-		{
-			$cookies = array();
-			foreach ( $_COOKIE as $key => $value )
-			{
-				$cookies[] = $key . '=' . $value;
-			}
 
-			$this->set_header('Cookie: ' . implode('; ', $cookies));
+
 		}
 	}
 
-	// @todo move to abstract?
-	public function copy_headers()
 	{
-		foreach ( $_SERVER as $key => $value )
-		{
-			if ( 'HTTP_' == substr($key, 0, 5) )
-			{
-				$key =	ucwords(
-							strtolower(
-								str_replace('_', '-',
-									str_replace('HTTP_', '', $key)
-								)
-							)
-						);
-
-				if ( $key != 'Cookie' )
-				{
-					$this->set_header($key . ': ' . $value);
-				}
-			}
-		}
-	}
-
-	// @todo write function
-	public function get_headers()
-	{
-		// Not yet implemented; see CURLOPT_HEADERFUNCTION in PHP manual.
 	}
 
 	// @todo Allow option specification?
@@ -95,25 +40,11 @@ class FuriCurl_Core extends FuriAbstract implements FuriInterface {
 		return $this->getinfo();
 	}
 
-	// @todo write function; options will need to be stored in a temp array
-	public function get_options()
+
 	{
-		// Not yet implemented
 	}
 
-	public function set_header($header)
 	{
-		// Not yet implemented
-	}
-
-	public function set_option($option, $value)
-	{
-		$this->setopt($option, $value);
-	}
-
-	public function set_options($options)
-	{
-		$this->setopt_array($options);
 	}
 
 // ============================================================================
