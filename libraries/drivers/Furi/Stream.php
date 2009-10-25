f286867bb8fc81495805a9abe6e9a365e1f64085
Beau Dacious <beau@cxzcxz.com>
2009-10-25 13:47:22 -0400

    bare bones Stream content fetching

--- libraries/drivers/Furi/Stream.php
+++ libraries/drivers/Furi/Stream.php
@@ -6,5 +6,28 @@
  * @author   Beau Dacious <beau@cxzcxz.com>
  */
 class Furi_Stream_Driver extends Furi_Driver {
+	
+	// Stream context options
+	protected $options = array(
+		'http' => array(
+			'method' => 'GET',
+		)
+	);
+	
+/* ----------------------------------------------------------------------------
+	Interface Methods
+---------------------------------------------------------------------------- */
+	
+	public function get($uri)
+	{
+		$context = stream_context_create($this->options);
+		$handle = fopen($uri, 'rb', FALSE, $context);
+		
+		$content = stream_get_contents($handle);
+		
+		fclose($handle);
+		
+		return $content;
+	}
 
 } // End Furi_Stream_Driver
